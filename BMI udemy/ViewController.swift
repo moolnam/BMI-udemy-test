edb7d91212ca236872a80b0f2ae3254b21c47298
moolnam <moolnam>
2022-04-18 17:16:21 +0900

    weight slider changed, height slider changed.

--- BMI udemy/ViewController.swift
+++ BMI udemy/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        // 키 슬라이더
+    }
+    
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        // 몸무게 슬라이더
+    }
+    
 }
 
